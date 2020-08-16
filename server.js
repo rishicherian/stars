006db733872cf14db617eb4f1fc5c5b5b34f09d9
RishiC <rishi.cherian@hotmail.com>
2020-08-15 18:14:00 -0700

    Server fix

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const http = require('http').Server(server);
 var path = require('path');
 var serveStatic = require('serve-static');
 
+const port = process.env.PORT || 3000;
+
 // LocalTest: Comment next two statements for local test
 
 server.use(serveStatic(path.join(__dirname, '/client/public')))
@@ -12,5 +14,10 @@ server.use(serveStatic(path.join(__dirname, '/client/public')))
 server.get('/', (req, res) => {
     res.sendFile('index.html');
   });
+
+http.listen(port, function () {
+    console.log('Server started! Port=' + port);
+});
+
 
  
